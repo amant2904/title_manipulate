ca8709b2924e5343277d574ab0265f7cb76b8c32
amant2904 <tiwariaman2904@gmail.com>
2023-01-29 11:27:18 +0530

    delete and edit button added

--- dom.js
+++ dom.js
@@ -25,51 +25,84 @@
 // document.querySelector(".list-group-item:nth-child(3)").hidden = true;
 
 
-// parent Element
-console.log(document.getElementsByClassName("list-group-item")[0].parentElement);
+// // parent Element
+// console.log(document.getElementsByClassName("list-group-item")[0].parentElement);
 
-// parentNode
-document.getElementById("main").parentNode.parentNode.style.background = "grey";
+// // parentNode
+// document.getElementById("main").parentNode.parentNode.style.background = "g#ECECEC";
 
-// last child
-console.log(document.getElementById("items").lastChild);
+// // last child
+// console.log(document.getElementById("items").lastChild);
 
-// lastElementChild
-document.getElementById("items").lastElementChild.style.background = "skyblue";
+// // lastElementChild
+// document.getElementById("items").lastElementChild.style.background = "skyblue";
 
-// first element child
-document.querySelector("#items").firstElementChild.style.color = "purple";
+// // first element child
+// document.querySelector("#items").firstElementChild.style.color = "purple";
 
-// first child
-console.log(document.querySelectorAll("#items")[0].firstChild);
+// // first child
+// console.log(document.querySelectorAll("#items")[0].firstChild);
 
-// next sibling
-console.log(document.getElementsByClassName("title")[0].nextSibling);
+// // next sibling
+// console.log(document.getElementsByClassName("title")[0].nextSibling);
 
-// next element sibling
-document.getElementsByTagName("h2")[0].nextElementSibling.style.background = "#ECECEC";
+// // next element sibling
+// document.getElementsByTagName("h2")[0].nextElementSibling.style.background = "#ECECEC";
 
-// previous sibling
-console.log(document.getElementsByTagName("form")[0].previousSibling);
+// // previous sibling
+// console.log(document.getElementsByTagName("form")[0].previousSibling);
 
-// previous element sibling
-document.getElementsByTagName("form")[0].previousElementSibling.style.color = "red";
+// // previous element sibling
+// document.getElementsByTagName("form")[0].previousElementSibling.style.color = "red";
 
-// create element && append child
-let elem = document.createElement("h3");
-elem.innerText = "this is appended heading";
-document.getElementsByClassName("container")[1].appendChild(elem);
+// // create element && append child
+// let elem = document.createElement("h3");
+// elem.innerText = "this is appended heading";
+// document.getElementsByClassName("container")[1].appendChild(elem);
 
-// set attribute
-document.getElementsByTagName("form")[0].firstElementChild.setAttribute("name", "text");
+// // set attribute
+// document.getElementsByTagName("form")[0].firstElementChild.setAttribute("name", "text");
 
 
 
-let head = document.createElement("h2");
-head.innerText = "HEllo word";
-document.getElementsByClassName("container")[0].prepend(head);
+// let head = document.createElement("h2");
+// head.innerText = "HEllo word";
+// document.getElementsByClassName("container")[0].prepend(head);
 
-let new_item = document.createElement("li");
-new_item.textContent = "extra item added";
-new_item.className = "list-group-item";
-document.getElementById("items").prepend(new_item);
+// let new_item = document.createElement("li");
+// new_item.textContent = "extra item added";
+// new_item.className = "list-group-item";
+// document.getElementById("items").prepend(new_item);
+
+
+document.getElementById("items").addEventListener('click', (e) =>{
+    if(e.target.classList.contains("delete_btn")){
+        if(confirm("are you sure ?")){
+            e.target.parentElement.remove();
+        }
+    }
+})
+
+document.getElementById("submit").addEventListener('click', (e) =>{
+    e.preventDefault();
+    // create list item
+    let vll = document.getElementById("list_value").value;
+    let new_li = document.createElement("li");
+    new_li.appendChild(document.createTextNode(vll));
+    new_li.className = "list-group-item";
+    
+    // create delete button
+    let d_btn = document.createElement("button");
+    d_btn.appendChild(document.createTextNode("X"));
+    d_btn.className = "btn btn-danger mx-2 float-right delete_btn";
+    new_li.appendChild(d_btn);
+    
+    // create Edit button
+    let edit = document.createElement("button");
+    edit.appendChild(document.createTextNode("Edit"));
+    edit.className = "btn btn-warning float-right";
+    new_li.appendChild(edit);
+    
+    // append item in list
+    document.getElementById("items").append(new_li);
+})
